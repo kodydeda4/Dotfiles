d617e646f5efaef52698de9d04981139a8147e06
Kody Deda <kodydeda4@gmail.com>
2024-05-13 19:46:33 -0400

    update

--- nvim/lua/kody/plugins/xcodebuild.lua
+++ nvim/lua/kody/plugins/xcodebuild.lua
@@ -5,6 +5,11 @@ return {
   dependencies = {
     "nvim-telescope/telescope.nvim",
     "MunifTanjim/nui.nvim",
+    -- "nvim-telescope/telescope.nvim",
+    -- "MunifTanjim/nui.nvim",
+    -- "nvim-tree/nvim-tree.lua", -- (optional) to manage project files
+    -- "stevearc/oil.nvim", -- (optional) to manage project files
+    "nvim-treesitter/nvim-treesitter", -- (optional) for Quick tests support (required Swift parser)
   },
   config = function()
     require("xcodebuild").setup({
